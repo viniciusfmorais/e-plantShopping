fce398e4f510beeac8c26e815cedba85e3132f60
Vinicius <viniciusfmorais>
2025-03-07 11:20:59 +0000

    Fix GitHub Pages setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "/e-plantShopping/", // Make sure to include trailing slash
   plugins: [react()],
   server: {
     port: 3000,
