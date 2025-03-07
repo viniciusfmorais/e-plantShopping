93194b2599c4013a1d914076f4d7b9603aced5fc
Vinicius <viniciusfmorais>
2025-03-07 11:53:54 +0000

    Fix GitHub Pages setup

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,7 +274,7 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/paradise-nursery-shopping-cart-app/" style={{ textDecoration: 'none' }}>
+                        <a href="#" onClick={(e) => handlePlantsClick(e)} style={{ textDecoration: 'none' }}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Milan's Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
